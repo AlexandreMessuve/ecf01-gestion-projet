559368b558d8d19e80adf15ed1b5a05e78d5d720
Alexandre MESSUVE <alexandre.messuves@gmail.com>
2024-02-07 14:01:00 +0100

    update routes index.js

--- api-rest/routes/index.js
+++ api-rest/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const authRoute = require('./authRoutes');
 const projectRoutes = require('./projectRoutes');
+const taskRoutes = require('./taskRoutes');
 const isAuth = require('../middleware/isAuthentificated');
 
 router.use('/', authRoute);
 router.use('/api/projects', isAuth, projectRoutes);
+router.use('/api/tasks', isAuth, taskRoutes);
 
 module.exports = router;
