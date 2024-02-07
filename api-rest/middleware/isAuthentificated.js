3f74048503e3b7d183f731716cff20639d5d70be
Alexandre MESSUVE <alexandre.messuves@gmail.com>
2024-02-07 12:10:42 +0100

    add middleware isAuthentificated

---
+++ api-rest/middleware/isAuthentificated.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const  isAuthenticated =  (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1]
+        const decodedToken = jwt.verify(token, process.env.JWT_PASSPHRASE);
+        const userId = decodedToken.userId;
+        req.auth = {
+            userId: userId,
+        }
+        return next();
+    } catch(error) {
+        console.log(error);
+        res.status(401).json({message : "Merci de vous connecter"})
+    }
+}
+
+
+
+module.exports = isAuthenticated;
