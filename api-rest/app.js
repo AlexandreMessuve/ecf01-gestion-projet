c75c925336f107cdac9525f6faeb56ccd3a29959
Alexandre MESSUVE <alexandre.messuves@gmail.com>
2024-02-07 14:01:21 +0100

    add app.js

---
+++ api-rest/app.js
@@ -0,0 +1,25 @@
+require('dotenv/config');
+const express = require('express');
+const DB = require('./config/db');
+const allRoutes = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json()).use('/', allRoutes);
+
+
+DB.sequelize
+    .authenticate()
+    .then(() => console.log('Connexion à la base de données réussie'))
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Serveur sur écoute sur le port: ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.log(
+            'Erreur lors de la connexion à la base de données',
+            err.message
+        );
+    });
